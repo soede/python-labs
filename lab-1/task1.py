53e872785bbef6f19ed084e90597a930e22d6a6e
adam <adamnagoy@gmail.com>
2025-03-14 21:39:14 +0300

    added find_coprime_divisors

--- lab-1/task1.py
+++ lab-1/task1.py
@@ -19,7 +19,28 @@ def five_divisible(a):
     return mult
 
 
+def sum_of_numbers(a):
+    s = 0
+    for i in str(a):
+        s += int(i)
+    return s
+
+
+def find_coprime_divisors(n):
+    s = sum_of_numbers(n)
+    divisors = [i for i in range(1, n+1) if n % i == 0]
+
+    def gcd(a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
+    coprime_divisors = [divisor for divisor in divisors if gcd(divisor, s) == 1]
+    return coprime_divisors
+
+
 value = input('write some value: \n')
 
 print("Largest prime divisor of a number", max_prime_divisor(int(value)))
 print("The product of numbers divisible by 5: ", five_divisible(int(value)))
+print("Largest prime divisor of sum of the number: ", find_coprime_divisors(int(value)))
