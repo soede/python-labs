1573858221c1fabb39dd87a2c705ee6234937ae8
adam <adamnagoy@gmail.com>
2025-03-16 18:54:24 +0300

    added count_words

--- lab-1/task2.py
+++ lab-1/task2.py
@@ -1,12 +1,19 @@
 import random
 
 
-def shuffle_words(sentence: str) -> str:
+def shuffle_words(sentence):
     words = sentence.split()
     random.shuffle(words)
     return ' '.join(words)
 
 
-s = input("write text for shuffle")
+def count_words(sentence):
+    words = sentence.split()
+    return sum(1 for word in words if len(word) % 2 == 0)
+
+
+s = input("write something")
 shuffled = shuffle_words(s)
+count = count_words(s)
 print(shuffled)
+print(count)
