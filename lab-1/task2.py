ae73551a4dbaf5c5811115b7c6733d0a2bde8a0f
adam <adamnagoy@gmail.com>
2025-03-16 23:55:25 +0300

    added sort_flag

--- lab-1/task2.py
+++ lab-1/task2.py
@@ -12,8 +12,18 @@ def count_words(sentence):
     return sum(1 for word in words if len(word) % 2 == 0)
 
 
+def sort_flag(colors):
+    counts = {"белый": 0, "синий": 0, "красный": 0}
+
+    for color in colors:
+        counts[color] += 1
+
+    return ["белый"] * counts["белый"] + ["синий"] * counts["синий"] + ["красный"] * counts["красный"]
+
+
 s = input("write something")
 shuffled = shuffle_words(s)
 count = count_words(s)
 print(shuffled)
 print(count)
+print(sort_flag)
