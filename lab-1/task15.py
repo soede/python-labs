e6642798beef4756702dc728f5e4272e6830585a
adam <adamnagoy@gmail.com>
2025-03-23 21:36:55 +0300

    added 15 for 15 task

--- lab-1/task15.py
+++ lab-1/task15.py
@@ -1,21 +1,23 @@
 def is_global_max(arr, index):
     if index < 0 or index >= len(arr):
         return False
-
     value_at_index = arr[index]
-
     for i in range(len(arr)):
         if arr[i] > value_at_index:
             return False
-
     return True
 
-
+def is_local_min(arr, index):
+    if index < 0 or index >= len(arr):
+        return False
+    left = arr[index - 1] if index > 0 else float('inf')
+    right = arr[index + 1] if index < len(arr) - 1 else float('inf')
+    return arr[index] < left and arr[index] < right
 
 def main():
     task = input("Введи номер задания: (3, 15) ")
 
-    if task not in {"3", "14"}:
+    if task not in {"3", "15"}:
         print("Неправильный номер задания")
         return
 
@@ -27,15 +29,18 @@ def main():
             break
         arr.append(int(line))
 
+    index = int(input("Введи индекс: "))
+
     if int(task) == 3:
-        index = int(input("Введи индекс: "))
         if is_global_max(arr, index):
             print("Это глобальный максимум")
         else:
-            print("Это не глобальный максимум ")
-
-        return
-
+            print("Это не глобальный максимум")
+    elif int(task) == 15:
+        if is_local_min(arr, index):
+            print("Это локальный минимум")
+        else:
+            print("Это не локальный минимум")
 
 if __name__ == "__main__":
     main()
