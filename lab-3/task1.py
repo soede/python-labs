ddb02197975959151848ce333513d62a402ab31e
adam <adamnagoy@gmail.com>
2025-04-06 22:50:40 +0300

    add Pentagon class

--- lab-3/task1.py
+++ lab-3/task1.py
@@ -1,3 +1,4 @@
+import math
 from abc import ABC, abstractmethod
 
 
@@ -18,3 +19,21 @@ class Shape(ABC):
     @abstractmethod
     def is_include(self, other):
         pass
+
+
+class Pentagon(Shape):
+    def __init__(self, identifier, x, y, radius):
+        super().__init__(identifier, x, y)
+        self.radius = radius
+
+    def is_intersect(self, other):
+        if isinstance(other, Pentagon):
+            distance = math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
+            return distance < (self.radius + other.radius)
+        return False
+
+    def is_include(self, other):
+        if isinstance(other, Pentagon):
+            distance = math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
+            return distance + other.radius <= self.radius
+        return False
