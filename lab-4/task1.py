306b7bde60035b89eb09d33836bd9ce730734376
adam <adamnagoy@gmail.com>
2025-04-07 00:18:46 +0300

    add task1 for lab-4

---
+++ lab-4/task1.py
@@ -0,0 +1,19 @@
+def find_max_sum(filename):
+    with open(filename) as f:
+        N, K = map(int, f.readline().split())
+        values = [int(f.readline()) for _ in range(N)]
+
+    best1 = [0] * N
+    best1[0] = values[0]
+    for i in range(1, N):
+        best1[i] = max(best1[i-1], values[i])
+
+    best2 = [0] * N
+    for i in range(K, N):
+        best2[i] = max(best2[i-1], values[i] + best1[i-K])
+
+    max_sum = 0
+    for i in range(2*K, N):
+        max_sum = max(max_sum, values[i] + best2[i-K])
+
+    return max_sum
