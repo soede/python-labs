0cf20b5838fc75c5e1c8bfbebf491ecc44b7f994
adam <adamnagoy@gmail.com>
2025-04-06 23:13:04 +0300

    add Rectangle class

--- lab-3/task1.py
+++ lab-3/task1.py
@@ -37,3 +37,26 @@ class Pentagon(Shape):
             distance = math.sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
             return distance + other.radius <= self.radius
         return False
+
+
+class Rectangle(Shape):
+    def __init__(self, identifier, x, y, width, height):
+        super().__init__(identifier, x, y)
+        self.width = width
+        self.height = height
+
+    def is_intersect(self, other):
+        if isinstance(other, Rectangle):
+            return not (self.x + self.width < other.x or
+                        self.x > other.x + other.width or
+                        self.y + self.height < other.y or
+                        self.y > other.y + other.height)
+        return False
+
+    def is_include(self, other):
+        if isinstance(other, Rectangle):
+            return (self.x <= other.x and
+                    self.y <= other.y and
+                    self.x + self.width >= other.x + other.width and
+                    self.y + self.height >= other.y + other.height)
+        return False
